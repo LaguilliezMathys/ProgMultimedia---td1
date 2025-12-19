3d93e5c37625eb668320c104634781bd94b38a3b
Laguilliez <laguilliezmathys1@gmail.com>
2025-12-19 10:58:52 +0100

    ajout de la ballle + physique

--- breakout/control_joueur.gd
+++ breakout/control_joueur.gd
@@ -5,9 +5,16 @@ extends Node
 
 func _physics_process(delta):
 	var raquette : CharacterBody2D = get_parent()
-	if Input.action_press("p1_left"):
-		raquette.velocity.x =  speed
-	elif Input.action_press("p1_right"):
-			raquette.velocity.y = -speed
+	
+	raquette.velocity.y = 0
+	raquette.velocity.x = 0
+	
+	 
+	
+	
+	if Input.is_action_pressed("p1_left"):
+		raquette.velocity.x =  -speed
+	elif Input.is_action_pressed("p1_right"):
+		raquette.velocity.x = speed
 	
 	raquette.move_and_slide()
